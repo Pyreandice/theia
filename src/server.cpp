28e7014abd24a220b6a5159bba16622b42a68c44
Lewis Brown <lcb005@uark.edu>
2018-10-28 21:51:41 -0500

    updated server comment command logic + gitignore problem update (WIP)

--- src/server.cpp
+++ src/server.cpp
@@ -43,13 +43,6 @@ void Server::serverRespondLoop()
 
 bool Server::handleMessage(char * message)
 {
-  // \0 - initialized
-  //  0 - request
-  //  1 - response
-  // char messageType = message[0];
-
-  std::cout << "message: " << message << std::endl;
-
   // make sure the messageType is set
   if(* message == 0)
   {
@@ -58,10 +51,97 @@ bool Server::handleMessage(char * message)
     return false;
   }
 
+  // [MESSAGE DEFINITIONS]
+  // ------------------
+  //  0 - REQUEST
+  //  1 - RESPONSE
   char messageType = message[0];
-  std::string operation = this->getSubString(message, 1, 12);
 
-  std::cout << "operation: " << operation << std::endl;
+  // ---------------------------------------------------------------------------
+  // [KEY HANDSHAKE]
+  // ---------------------------------------------------------------------------
+  // (TO SERVER): HELLO
+  //  - CLIENT WRITES HELLO TO SERVER
+  //  - ARGS:
+  //    - SPECIFIC PUBLIC KEY
+  // (TO CLIENT): HELLO
+  //  - SERVER RESPONDS WITH HELLO TO CLIENT
+  //  - ARGS:
+  //    - NEWLY CREATED PUBLIC KEY FOR THAT SPECIFIC CLIENT
+  //  - REQUIREMENTS:
+  //    - SERVER MUST GENERATE PRIVATE/PUBLIC KEY PAIR
+  //    - SERVER MUST STORE PRIVATE/PUBLIC KEY PAIR IN MEMORY
+  //    - SERVER MUST POCESS MEMORY ON A SOCKET TO SOCKET BASIS
+  //    - SERVER AND CLIENT ARE REQUIRED TO USE ENCRYPTED MESSAGES FROM HERE ON
+
+  // ---------------------------------------------------------------------------
+  // [PRIVLEDGE ESCALATION]
+  // ---------------------------------------------------------------------------
+  // (TO SERVER): AUTH
+  // - CLIENT WRITES AUTH TO SERVER
+  // - ARGS:
+  //    - VALUE OF AUTH LEVEL [256-bit key] => LEVEL [0-3]
+  // (TO CLIENT): [SUCCESS/FAILURE]
+  // - SERVER WRITES OPERATION RESULT TO CLIENT
+  //    - FAILURE
+  //    - SUCCESS
+
+  // ---------------------------------------------------------------------------
+  // [SET [VAR]]
+  // ---------------------------------------------------------------------------
+  // (TO SERVER): SET [VAR] [VALUE]
+  // - CLIENT WRITES SET COMMAND TO SERVER
+  // - ARGS:
+  //    - VARIABLE NAME
+  //    - VARIABLE VALUE
+  //    - (VAR = "STATE"; VALUE = ?)
+  //      - CLOSED
+  //      - READY
+  //      - WORKING
+  // (TO CLIENT): [SUCCESS/FAILURE]
+  // - SERVER WRITES OPERATION RESULT TO CLIENT
+  //    - FAILURE [REASON]
+  //    - SUCCESS
+
+  // ---------------------------------------------------------------------------
+  // [GET [VAR]]
+  // ---------------------------------------------------------------------------
+  // (TO SERVER): GET [VAR]
+  // - CLIENT WRITES GET COMMAND TO SERVER
+  // - ARGS:
+  //    - VARIABLE NAME
+  // (TO CLIENT): RETURN [VAR VALUE]
+  // - SERVER WRITES RETURN TO CLIENT
+  // - ARGS:
+  //    - VAR VALUE
+
+  // ---------------------------------------------------------------------------
+  // [CLOSING CONNECTION]
+  // ---------------------------------------------------------------------------
+  // (TO CLIENT): GOODBYE
+  // - SERVER WRITES GOODBYE TO CLIENT
+  // (CLIENT ACTION):
+  // - CLOSES CONNECTION
+  // (SERVER AUTO CLOSES CONNECTION WHEN SOCKET DROPPED)
+
+  // ---------------------------------------------------------------------------
+  // [EXPORTING DATA]
+  // ---------------------------------------------------------------------------
+  // (TO CLIENT): EXPORT [EXPORTTYPE]
+  // - SERVER WRITES EXPORT TO CLIENT
+  // - ARGS:
+  //    - EXPORTTYPE - TYPE OF EXPORT CLIENT SHOULD PERFORM
+  // (CLIENT ACTION) POST REQUEST TO URL - https://curl.haxx.se/libcurl/c/http-post.html
+
+  // ---------------------------------------------------------------------------
+  // [IMPORTING DATA]
+  // ---------------------------------------------------------------------------
+  // (TO CLIENT): IMPORT [TYPE] []
+  // - SERVER WRITES EXPORT TO CLIENT
+  // - ARGS:
+  //    - TYPE - TYPE OF EXPORT CLIENT SHOULD PERFORM
+  
+  std::string command = this->getSubString(message, 1, 12);
 
   this->write(this->clientFileDescriptor, message);
 
